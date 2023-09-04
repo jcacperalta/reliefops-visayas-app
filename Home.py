60716e29086a040f1005d4b55b58648a13b67a87
JC Albert Peralta <jcacperalta@gmail.com>
2023-09-04 18:10:48 +0800

    Initial commit

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title='ReliefOps Visayas Transport Network Efficiency Simulator')
+
+st.title('ReliefOps Visayas Transport Network Efficiency Simulator')
+st.markdown("---")
+st.markdown("### About the project")
+st.markdown("### The tool")
+st.markdown("### Meet the team")
